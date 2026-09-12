942da8b1ac41b6ebbc0fef63197c5a4e46a0cdbf
Tomithy the One True Tom <cyril.saroch@ava.uk>
2026-09-12 17:40:40 +0100

    add hardware config file

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -2,10 +2,10 @@
 
 {
   imports = [
-	/etc/nixos/hardware-configuration.nix
-	./immutable-config.nix # Stuff which changes very rarely
-	./users.nix # User configuration
-	./wayland.nix
+    /etc/nixos/hardware-configuration.nix
+    ./immutable-config.nix # Stuff which changes very rarely
+    ./users.nix # User configuration
+    ./wayland.nix
   ];
 
   # Which packages to install system-wide
@@ -16,8 +16,8 @@
     git
     firefox
     terminator
-	flameshot # Screenshots
-	wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
+    flameshot # Screenshots
+    wl-clipboard # wl-copy and wl-paste for copy/paste from stdin / stdout
     foot # Sway terminal
     waybar # Sway status bar
     wofi # App launcher
@@ -34,18 +34,16 @@
     eza # Better ls
     fzf # Fuzzy finder
     carapace
-	nnn # terminal file manager
-	zip
-	unzip
-	dnsutils
-	file
-	which
-	tree
-	whereis
-	nix-output-monitor # "nom" is like "nix" but better output
-	glow # markdown highlighting in terminal
-	top
-	lsof
+    nnn # terminal file manager
+    zip
+    unzip
+    dnsutils
+    file
+    tree
+    whereis
+    nix-output-monitor # "nom" is like "nix" but better output
+    glow # markdown highlighting in terminal
+    lsof
   ];
 
   # TODO This is hella unfinished.
--- nix/immutable-config.nix
+++ nix/immutable-config.nix
@@ -32,15 +32,29 @@
   networking.networkmanager.enable = true;
 
   # Use the GRUB 2 boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.useOSProber = true;
   # Limit the number of generations to keep
   boot.loader.systemd-boot.configurationLimit = 10;
 
-  i18n.defaultLocale = "en_US.UTF-8";
   time.timeZone = "Europe/London";
   services.xserver.xkb.layout = "us";
   services.printing.enable = true;
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  i18n.extraLocaleSettings = {
+	LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
 
   nixpkgs.config.allowUnfree = true;  
 
@@ -48,6 +62,24 @@
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
   system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
 
   # TODO: This is where system.stateVersion goes. That isn't safe to just change
   # on my own, so let the OS generate it, then fill it in here, then remove the
