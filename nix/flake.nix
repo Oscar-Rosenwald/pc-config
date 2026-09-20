a3ea6165f1031ded9c144d04df99c0b0f460b9de
Tomithy the One True Tom <cyril.saroch@ava.uk>
2026-09-20 18:21:17 +0100

    wip: no need for depending explicitly on oneself

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,59 +1,131 @@
 {
-  description = "Core Computer Configuration";
+  description = "Configuration. That's it. Fuck you.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-    myConfigDir.url = "github:Oscar-Rosenwald/pc-config/test";
-    home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+	nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+	home-manager = {
+	  url = "github:nix-community/home-manager/release-26.05";
+	  inputs.nixpkgs.follows = "nixpkgs";
+	};
 
-    # For other things to pass to the output modules, do something like this:
-    # 
-    #   helix.url = "github:helix-editor/helix/master";
-    #
-    # (Add helix.flake = false if it's just a repo with no flake.nix) Then in
-    # outputs.nixosConfiguration.<name>, on the same level as 'modules,' add this:
-    #
-    #   specialArgs = { inherit inputs; };
-    #
-    # This will make the package "helix" available in configuration.nix under an
-    # argument called "inputs" like: inputs.helix. If this doesn't work (I've
-    # not been able to figure out why this is), use "specialArgs" instead of
-    # "inputs" in the target module. I really don't get why it's inputs
-    # sometimes and not other times.
+	# For other things to pass to the output modules, do something like this:
+	# 
+	#   helix.url = "github:helix-editor/helix/master";
+	#
+	# (Add helix.flake = false if it's just a repo with no flake.nix) Then in
+	# outputs.nixosConfiguration.<name>, on the same level as 'modules,' add this:
+	#
+	#   specialArgs = { inherit inputs; };
+	#
+	# This will make the package "helix" available in configuration.nix under an
+	# argument called "inputs" like: inputs.helix. If this doesn't work (I've
+	# not been able to figure out why this is), use "specialArgs" instead of
+	# "inputs" in the target module. I really don't get why it's inputs
+	# sometimes and not other times.
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
-    let
-      system            = "x86_64-linux";
-      pkgs              = import nixpkgs { inherit system; config.allowUnfree = true; };
-      customConfig      = inputs.myConfigDir;
-      configFiles       = customConfig.files;
-      configValues      = customConfig.values;
-      hardwareConfigs   = customConfig.hardware;
-      configUtils       = customConfig.utils;
-      specialArgs       = {
-        inherit inputs;
-        inherit configValues;
-        inherit configFiles;
-      };
-      # buildHostConfiguration :: hardwareName -> hardwareConfig -> hostname-nixosConfig
-      buildHostConfiguration = configUtils.buildHostConfiguration {
-        inherit specialArgs;
-        inherit home-manager;
-        inherit nixpkgs;
-        configurationFile = ./configuration.nix;
-      };
-    in {
-      nixosConfigurations = 
-        # concatMapAttrs takes the set hardwareConfig, and calls
-        # buildHostConfiguration on each of its attributes. The result is a list
-        # of sets, which are merged.
+  outputs = { self, nixpkgs, home-manager, ... }@inputs:
+	let
+	  system            = "x86_64-linux";
+	  pkgs              = import nixpkgs { inherit system; config.allowUnfree = true; };
+	  configFiles       = self.outputs.files;
+	  configValues      = self.outputs.values;
+	  utils             = import ./utils.nix;
+	  specialArgs       = {
+		inherit inputs;
+		inherit configValues;
+		inherit configFiles;
+	  };
+	  # buildHostConfiguration :: hardwareName -> hardwareConfig -> hostname-nixosConfig
+	  buildHostConfiguration = utils.buildHostConfiguration {
+		configurationFile = configFiles.masterConfig;
+		inherit specialArgs;
+		inherit home-manager;
+		inherit nixpkgs;
+	  };
+	in {
+	  # Holds paths to files which the caller (home-manager config) needs. This
+	  # should guarantee that the files exist.
+	  files = {
+		# These are used in the nushell config text, so we must ensure they
+		# exist.
+		nushell = {
+		  aliases = ./../nushell/aliases.nu;
+		  chdir = ./../nushell/chdir.nu;
+		  completions = ./../nushell/completions.nu;
+		  docker = ./../nushell/docker.nu;
+		  external = ./../nushell/external.nu;
+		  git = ./../nushell/git.nu;
+		  grep = ./../nushell/grep.nu;
+		  jj = ./../nushell/jj.nu;
+		  kubectl = ./../nushell/kubectl.nu;
+		  loadEnv = ./../nushell/load_env.nu;
+		  misc = ./../nushell/misc.nu;
+		  notes = ./../nushell/notes.org;
+		  resiliency = ./../nushell/resiliency.nu;
+		  screens = ./../nushell/screens.nu;
+		  ssh = ./../nushell/ssh.nu;
+		  testsMod = ./../nushell/tests_mod.nu;
+		  updatePath = ./../nushell/update_path.nu;
+		  vms = ./../nushell/vms.nu ;
+		};
+
+		jujutsu = ./../jj/config.toml;
+		vim = ./../vim;
+		dunst = ./../dunst;
+		terminator = ./../terminator;
+		masterConfig = ./configuration.nix;
+	  };
+
+	  # Holds random values which the caller (home-manager config) needs. These
+	  # values are derived somehow from the files in the 'files' set above, so
+	  # they must be defined here as well.
+	  values = {
+		ssh =
+		  let
+			sshDir = "~/.ssh";
+		  in {
+			# The identity file used by default and for private github.
+			defaultIdentity = "${sshDir}/default";
+
+			# The identity file used at work for contacting Motorola's GitHub or
+			# GitLab.
+			workIdentity = "${sshDir}/work";
+		  };
+	  };
+
+	  # Contains information local to specific hosts, i.e., specific machines.
+	  hardware = {
+		# The qemu virtual machine.
+		qemuVm = {
+		  hostname = "qemu-william";
+		  hardwareConfiguration = ./qemu-hardware-configuration.nix;
+		  bootloaderDevice = "/dev/sda";
+		};
+
+		# The qemu virtual machine on the work laptop.
+		workQemuVm = {
+		  hostname = "work-qemu-william";
+		  hardwareConfiguration = ./work-qemu-hardware-configuration.nix;
+		  bootloaderDevice = "/dev/vda1";
+		};
+
+		# Any machine whose hostname we wish to change. Need to build-switch nixos
+		# with this, then reboot, and the machine will have a different hostname.
+		# Feel free to change [hostname] (the current one) and [hostnameOverride]
+		# (the target hostname) to do this. No machine will (or can) use this
+		# config long-term.
+		hostRename = self.hardware.workQemuVm // {
+		  hostname = "mountain-lightning-on-the-net";
+		  hostnameOverride = "work-qemu-william";
+		};
+	  };
+
+	  nixosConfigurations =
+		# concatMapAttrs :: (name -> value -> {mapName: mapValue}) -> {name: value} -> {mapName: mapValue}
 		#
 		# This will produce a set of nixosConfigurations where each attribute is
-        # a hostname and contains the hosts's configuration.
-        pkgs.lib.attrsets.concatMapAttrs buildHostConfiguration hardwareConfigs;
-    };
+		# a hostname and contains the hosts's configuration.
+		pkgs.lib.attrsets.concatMapAttrs buildHostConfiguration self.outputs.hardware;
+	};
 }
--- nix/home.nix
+++ nix/home.nix
@@ -29,157 +29,150 @@ in {
   # This comes from carapace's docs. We must produce the init.nu file for
   # nushell so that it can call it with command-line arguments.
   home.file.".cache/nushell/carapace/init.nu".source = pkgs.runCommand "carapace-init" {} ''
-    ${pkgs.carapace}/bin/carapace _carapace nushell > $out
+	${pkgs.carapace}/bin/carapace _carapace nushell > $out
   '';
 
   # Nushell configuration
   programs.nushell = {
-    enable = true;
+	enable = true;
 
-    configFile.text =
-      let
-        files = configFiles.nushell;
-      in ''
-        source ${files.loadEnv}
-        source ${files.updatePath}
+	configFile.text =
+	  let
+		files = configFiles.nushell;
+	  in ''
+		source ${files.loadEnv}
+		source ${files.updatePath}
 
-        source ${files.misc}
-        source ${files.chdir}
-        source ${files.docker}
-        source ${files.vms}
-        source ${files.external}
+		source ${files.misc}
+		source ${files.chdir}
+		source ${files.docker}
+		source ${files.vms}
+		source ${files.external}
 
-        use ${files.git} *
-        use ${files.grep} *
-        use ${files.testsMod} *
-        use ${files.screens} *
-        use ${files.resiliency} *
-        use ${files.jj} *
-        use ${files.ssh} *
-        use ${files.kubectl} *
+		use ${files.git} *
+		use ${files.grep} *
+		use ${files.testsMod} *
+		use ${files.screens} *
+		use ${files.resiliency} *
+		use ${files.jj} *
+		use ${files.ssh} *
+		use ${files.kubectl} *
 
-        source ${files.aliases}
-    '';
+		source ${files.aliases}
+	  '';
 
-    # This completion config comes from carapace's docs. See above for where
-    # init.nu comes from.
-    extraConfig = "source ($nu.cache-dir | path join carapace/init.nu)";
-    environmentVariables.CARAPACE_BRIDGES = "'zsh,fish,bash,inshellisense'";
+	# This completion config comes from carapace's docs. See above for where
+	# init.nu comes from.
+	extraConfig = "source ($nu.cache-dir | path join carapace/init.nu)";
+	environmentVariables.CARAPACE_BRIDGES = "'zsh,fish,bash,inshellisense'";
 
-    settings = {
-      show_banner = false;
-      history = {
-        file_format = "sqlite";
-        isolation = true;
-      };
-    };
+	settings = {
+	  show_banner = false;
+	  history = {
+		file_format = "sqlite";
+		isolation = true;
+	  };
+	};
   };
 
   programs.bash = {
-    enable = true;
-    enableCompletion = true;
+	enable = true;
+	enableCompletion = true;
 
-    # TODO Do we need to do this?
-    # Configuring right alt to be the compose key. There are a few issues with it,
-    # so we need to do it in this weird way. Blame Gemini for this mess.
-    profileExtra = ''
-      export GTK_IM_MODULE=none
-      export QT_IM_MODULE=xim
-      export XMODIFIERS="@im=none"
-    '';
+	# TODO Do we need to do this?
+	# Configuring right alt to be the compose key. There are a few issues with it,
+	# so we need to do it in this weird way. Blame Gemini for this mess.
+	profileExtra = ''
+	  export GTK_IM_MODULE=none
+	  export QT_IM_MODULE=xim
+	  export XMODIFIERS="@im=none"
+	'';
 
-    shellAliases = {
-      "ls" = "ls --color=auto -G";
-      "ll" = "ls -alFvG";
-      "la" = "ls -Av";
-      "l"  = "ls -CFv";
-      "l1" = "ls -1vt";
-      "lt" = "ls -1vt";
-      "mv" = "mv -i";
-      "beep" = "paplay -n synth 0.2 sine 1000 vol 0.2";
-      "fireb" = "firefox -p blabla 2>/dev/null &";
-      "phones" = "bluetooth connect 2C:27:9E:71:45:69";
-    };
+	shellAliases = {
+	  "ls" = "ls --color=auto -G";
+	  "ll" = "ls -alFvG";
+	  "la" = "ls -Av";
+	  "l"  = "ls -CFv";
+	  "l1" = "ls -1vt";
+	  "lt" = "ls -1vt";
+	  "mv" = "mv -i";
+	  "beep" = "paplay -n synth 0.2 sine 1000 vol 0.2";
+	  "fireb" = "firefox -p blabla 2>/dev/null &";
+	  "phones" = "bluetooth connect 2C:27:9E:71:45:69";
+	};
   };
 
-  programs.jujutsu.enable = true;
   home.file.".config/jj/config.toml" = {
-    enable = true;
-    source = configFiles.jujutsu;
+	enable = true;
+	source = configFiles.jujutsu;
   };
 
   programs.vim = {
-    enable = true;
-    settings = {
-      tabstop = 4;
-      shiftwidth = 4;
-      expandtab = true;
-    };
-  };
-
-  home.file.".vim" = {
-    enable = true;
-    source = configFiles.vim;
+	enable = true;
+	settings = {
+	  tabstop = 4;
+	  shiftwidth = 4;
+	  expandtab = true;
+	};
   };
 
   programs.ssh = {
-    enable = true;
+	enable = true;
 
-    settings = {
-      # Credentials for my personal GitHub account.
-      "oscar.github.com" = {
-        HostName = "github.com";
-        IdentityFile = configValues.ssh.workIdentity;
-        ForwardAgent = "yes";
-      };
+	settings = {
+	  # Credentials for my personal GitHub account.
+	  "oscar.github.com" = {
+		HostName = "github.com";
+		IdentityFile = configValues.ssh.workIdentity;
+		ForwardAgent = "yes";
+	  };
 
-      # Credentials for my work GitHub account.
-      "tom.github.com" = {
-        HostName = "github.com";
-        PreferredAuthentications = "publickkey";
-        IdentityFile = configValues.ssh.defaultIdentity;
-      };
+	  # Credentials for my work GitHub account.
+	  "tom.github.com" = {
+		HostName = "github.com";
+		PreferredAuthentications = "publickkey";
+		IdentityFile = configValues.ssh.defaultIdentity;
+	  };
 
-      # Default credentials.
-      "*" = {
-        IgnoreUnknown = "UseKeychain";
-        AddKeysToAgent = "yes";
-        IdentityFile = configValues.ssh.defaultIdentity;
-        StrictHostKeyChecking = "no";
-      };
-    };
+	  # Default credentials.
+	  "*" = {
+		IgnoreUnknown = "UseKeychain";
+		AddKeysToAgent = "yes";
+		IdentityFile = configValues.ssh.defaultIdentity;
+		StrictHostKeyChecking = "no";
+	  };
+	};
   };
 
   programs.git = {
-    enable = true;
-    lfs.enable = true;
-    settings = {
-      user = {
-        email = "cyril.saroch@motorolasolutions.com";
-        name = [ "cyril.saroch" "Cyril" ];
-      };
-      url."git@repo.jazznetworks.com:".insteadOf = "https://repo.jazznetworks.com/";
-      core.editor = "emacsclient --create-frame";
-      # TODO git/contrib won't be in this path.
-      # credential.helper = /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret;
-      pull.rebase = false;
-      push.recurseSubmodules = false;
-      submodule.recurse = true;
-      init.defaultBranch = "master";
-    };
+	enable = true;
+	lfs.enable = true;
+	settings = {
+	  user = {
+		email = "cyril.saroch@motorolasolutions.com";
+		name = [ "cyril.saroch" "Cyril" ];
+	  };
+	  url."git@repo.jazznetworks.com:".insteadOf = "https://repo.jazznetworks.com/";
+	  core.editor = "emacsclient --create-frame";
+	  # TODO git/contrib won't be in this path.
+	  # credential.helper = /usr/share/doc/git/contrib/credential/libsecret/git-credential-libsecret;
+	  pull.rebase = false;
+	  push.recurseSubmodules = false;
+	  submodule.recurse = true;
+	  init.defaultBranch = "master";
+	};
   };
 
   home.file.".sqliterc".text = ''
-    .m box
+	.m box
   '';
 
   home.file.".config/dunst/".source = configFiles.dunst;
 
-  programs.terminator.enable = true;
   home.file.".config/terminator".source = configFiles.terminator;
 
   wayland.windowManager.sway = {
-    enable = true;
-    # TODO: config = { ... }; See home-manager/modules/services/window-managers/i3-sway/sway.nix
+	enable = true;
+	# TODO: config = { ... }; See home-manager/modules/services/window-managers/i3-sway/sway.nix
   };
 }
